7b4ad569efe65bc626b99ba2f001acacb381cb61
tedklin <tedklin@gmail.com>
2018-07-05 16:43:51 -0700

    refactoring

--- src/filters/g_h_filter.py
+++ src/filters/g_h_filter.py
@@ -1,15 +1,11 @@
-import numpy as np
-
-'''
-g-h filter on 1-state variable
-'''
+"""g-h filter on 1-state variable"""
 
 
 class GHFilter:
 
     def __init__(self, initial_val, initial_rate, g, h):
-        self.estimated_val = initial_val
-        self.estimated_rate = initial_rate
+        self._estimated_val = initial_val
+        self._estimated_rate = initial_rate
         self.g = g
         self.h = h
 
@@ -20,15 +16,24 @@ class GHFilter:
         self.h = h
 
     def update(self, measured_val, dt):
-        predicted_val = self.estimated_val + self.estimated_rate * dt
-        predicted_rate = self.estimated_rate
+        """
+        Update the filter with a new measurement. 'Prediction' also occurs in this function.
+        Does not return the estimated value. In some cases, it is beneficial to update the filter at a faster
+        rate than that with which you reference the value.
+
+        :param measured_val: new measured value
+        :param dt: difference in time between previous update step and current update step
+        :return: nothing; use get_estimation() to get the estimated value
+        """
+        predicted_val = self._estimated_val + self._estimated_rate * dt
+        predicted_rate = self._estimated_rate
 
         residual = measured_val - predicted_val
-        self.estimated_rate = predicted_rate + self.h * residual / dt
-        self.estimated_val = predicted_val + self.g * residual
+        self._estimated_rate = predicted_rate + self.h * residual / dt
+        self._estimated_val = predicted_val + self.g * residual
 
     def get_estimation(self):
-        return self.estimated_val
+        return self._estimated_val
 
     def get_rate(self):
-        return self.estimated_rate
+        return self._estimated_rate
